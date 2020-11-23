a78add33c8e445dea1bc6b0e606560df1d7ddf85
NastyaSvich <56764613+NastyaSvich@users.noreply.github.com>
2020-11-23 22:17:13 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,18 +5,16 @@ var express = require('express'),
   Product = require('./restapi/models/productModel'),
   Order = require('./restapi/models/orderModel'),
   bodyParser = require('body-parser'),
-  cors = require('cors');//mongodb://localhost/onlineСafe
-//mongodb://Swooopi:swOOOpi6362@cluster0-shard-00-00.pqxdz.mongodb.net:27017,cluster0-shard-00-01.pqxdz.mongodb.net:27017,cluster0-shard-00-02.pqxdz.mongodb.net:27017/onlineСafe?ssl=true&replicaSet=atlas-txlopl-shard-0&authSource=admin&retryWrites=true&w=majority
-mongoose.Promise = global.Promise; ///mongodb+srv://Swooopi:swOOOpi6362@cluster0.pqxdz.mongodb.net/onlineСafe?retryWrites=true&w=majority
-mongoose.connect('mongodb://Swooopi:swOOOpi6362@cluster0-shard-00-00.pqxdz.mongodb.net:27017,cluster0-shard-00-01.pqxdz.mongodb.net:27017,cluster0-shard-00-02.pqxdz.mongodb.net:27017/onlineСafe?ssl=true&replicaSet=atlas-txlopl-shard-0&authSource=admin&retryWrites=true&w=majority', { useMongoClient: true });
+  cors = require('cors'),
+  https = require('https'),
+  http = require('http'),
+  fs = require('fs');
 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://Swooopi:swOOOpi6362@cluster0.pqxdz.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect;
-
+mongoose.Promise = global.Promise; //mongodb://localhost/onlineСafe
+mongoose.connect('mongodb://Swooopi:swOOOpi6362@cluster0-shard-00-00.pqxdz.mongodb.net:27017,cluster0-shard-00-01.pqxdz.mongodb.net:27017,cluster0-shard-00-02.pqxdz.mongodb.net:27017/onlineСafe?ssl=true&replicaSet=atlas-txlopl-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true });
 
 app.use(cors());
+app.options('*', cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -25,10 +23,15 @@ var routesOrd = require('./restapi/routes/orderRoutes');
 routes(app);
 routesOrd(app);
 
-app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
-});
+// This line is from the Node.js HTTPS documentation.
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 
-app.listen(port);
+// Create an HTTP service.
+http.createServer(app).listen(80);
+// Create an HTTPS service identical to the HTTP service.
+https.createServer(options, app).listen(3000);
 
-console.log('Начинаем готовить пиццу :D -  Запуск на порту: ' + port);
+console.log('Начинаем готовить пиццу :D -  Запуск на порту: 3000');
