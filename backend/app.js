fa0684e6b5d09892028dc496c35c43d9b323b83c
Nastya Svich <nastyasvich60@gmail.com>
2020-11-23 22:34:21 +0300

    add config

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ var express = require('express'),
   cors = require('cors');
 
 mongoose.Promise = global.Promise; //mongodb://localhost/onlineСafe
-mongoose.connect('mongodb://Swooopi:swOOOpi6362@cluster0-shard-00-00.pqxdz.mongodb.net:27017,cluster0-shard-00-01.pqxdz.mongodb.net:27017,cluster0-shard-00-02.pqxdz.mongodb.net:27017/onlineСafe?ssl=true&replicaSet=atlas-txlopl-shard-0&authSource=admin&retryWrites=true&w=majority', { useMongoClient: true });
+mongoose.connect('mongodb://Swooopi:swOOOpi6362@cluster0-shard-00-00.pqxdz.mongodb.net:27017,cluster0-shard-00-01.pqxdz.mongodb.net:27017,cluster0-shard-00-02.pqxdz.mongodb.net:27017/onlineСafe?ssl=true&replicaSet=atlas-txlopl-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true });
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
